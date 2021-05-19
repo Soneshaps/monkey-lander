6c0a488fa45a788ae9dfa06abde465861688dbd3
soneshaps <sonesh.aps@gmail.com>
2021-05-19 18:42:40 +0545

    alignment fixed

--- js/fuel.js
+++ js/fuel.js
@@ -1,50 +1,54 @@
-class Fuel{
-    constructor(){
-        this.fuelImage = new Image()
-        this.fuelImage.src = 'images/fuel-bar.png'
-        this.width = 235
-        this.height = 27
-        this.x = 735
-        this.y = 65
+class Fuel {
+  constructor() {
+    this.fuelImage = new Image();
+    this.fuelImage.src = "images/fuel-bar.png";
+    this.width = 235;
+    this.height = 27;
+    this.x = 735;
+    this.y = 65;
 
-        this.fuelHealth = 228
+    this.fuelHealth = 300;
+  }
+  update(ctx) {
+    ctx.beginPath();
+    ctx.lineWidth = 2.5;
+    ctx.fillStyle = "#ffffff";
+    ctx.strokeStyle = "#000000";
+    ctx.moveTo(680, 70);
+    ctx.arcTo(980, 70, 980, 100, 15);
+    ctx.arcTo(980, 100, 666, 100, 15);
+    ctx.arcTo(666, 100, 666, 70, 15);
+    ctx.arcTo(666, 70, 980, 70, 15);
+    ctx.fill();
+    ctx.stroke();
+    ctx.closePath();
 
+    ctx.lineWidth = 1;
+
+    ctx.clearRect(672, 74, 300, 22);
+    ctx.beginPath();
+    ctx.fillStyle = "#66cc00";
+    ctx.rect(672, 74, this.fuelHealth, 22);
+    ctx.fill();
+    ctx.closePath();
+    ctx.font = "14px Nunito-Bold";
+    ctx.fillStyle = "#ffffff";
+    ctx.strokeStyle = "#ffffff";
+    ctx.strokeText("COCONUT FUEL", 770, 90);
+    ctx.fillText("COCONUT FUEL", 770, 90);
+
+    if (this.fuelHealth < 0) {
+      game.monkey.downThrust = 0;
+      game.monkey.xGravity = 0;
+      game.monkey.xRightGravity = 0;
+      game.monkey.xLeftSpeed = 0;
+      game.monkey.xRightSpeed = 0;
     }
-    update(ctx){
-        ctx.beginPath()
-        ctx.moveTo(680,70)
-        ctx.lineTo(800,70)
-        
-        ctx.closePath()
+  }
 
-
-
-        // ctx.clearRect(735, 60, 235 , 27);
-        ctx.beginPath();
-        ctx.fillStyle = "#66cc00";
-        ctx.rect(737.5, 64, this.fuelHealth, 20);
-        ctx.fill()
-        ctx.closePath();
-        ctx.font = "14px 'Nunito', sans-serif";
-        ctx.fillStyle = "#ffffff";
-        ctx.strokeStyle = "#ffffff";
-        ctx.strokeText("COCONUT FUEL", 780, 78);
-
-        if(this.fuelHealth < 0){
-            game.monkey.downThrust = 0
-            game.monkey.xGravity = 0
-            game.monkey.xRightGravity = 0
-            game.monkey.xLeftSpeed = 0
-            game.monkey.xRightSpeed = 0
-        }
- 
+  decreaseFuel() {
+    if (this.fuelHealth > 0) {
+      this.fuelHealth -= 0.5;
     }
-
-    decreaseFuel(){
-        if(this.fuelHealth > 0){
-            this.fuelHealth -= .2
-        }
-
-    }
-
+  }
 }
--- js/game.js
+++ js/game.js
@@ -1,164 +1,169 @@
-class Game{
-    constructor(context){
-        this.context = context
-        this.background = new Background()
-        this.monkeyLife = new MonkeyLife()
-        this.fuel = new Fuel()
-        this.monkey = new Monkey() 
-        this.landingSpace = new LandingSpace()
-        this.banana = new Banana()
-        this.bananaLeft = new BananaLeft()
-        this.score = new Score()
-        this.missionP = new MissionP()
-        this.level = 0
-        this.state = 1
-        this.gameClock = 0
-    }
-    update(){
-        if(this.state === 1){
-            this.gameClock += 1
-            this.background.update(this.context)
-            this.monkeyLife.update(this.context)
-            this.fuel.update(this.context)
-            this.bananaLeft.update(this.context)
-            this.banana.update(this.context)
-            // this.monkey.update(this.context)
-            this.score.update(this.context)
+class Game {
+  constructor(context) {
+    this.context = context;
+    this.background = new Background();
+    this.monkeyLife = new MonkeyLife();
+    this.fuel = new Fuel();
+    this.monkey = new Monkey();
+    this.landingSpace = new LandingSpace();
+    this.banana = new Banana();
+    this.bananaLeft = new BananaLeft();
+    this.score = new Score();
+    this.missionP = new MissionP();
+    this.level = 0;
+    this.state = 1;
+    this.gameClock = 0;
+  }
+  update() {
+    if (this.state === 1) {
+      this.gameClock += 1;
+      this.background.update(this.context);
+      this.monkeyLife.update(this.context);
+      this.fuel.update(this.context);
+      this.bananaLeft.update(this.context);
+      this.banana.update(this.context);
+      this.monkey.update(this.context);
+      this.score.update(this.context);
 
-            this.checkBorderCollision()
-            this.bananaCollision(this.monkey,this.banana)
-            
-            if(this.gameClock % 6 === 0){
-                this.banana.hang()
-            }
-            //after collecting all bananas
-            if(game.banana.bananaLeftToCollect === 0){
-                this.landingSpace.update(this.context)
-                this.landingCollision(this.monkey,this.landingSpace)
-            }
+      this.checkBorderCollision();
+      this.bananaCollision(this.monkey, this.banana);
+
+      if (this.gameClock % 6 === 0) {
+        this.banana.hang();
+      }
+      //after collecting all bananas
+      if (game.banana.bananaLeftToCollect === 0) {
+        this.landingSpace.update(this.context);
+        this.landingCollision(this.monkey, this.landingSpace);
+      }
+    }
+    if (this.state === 2) {
+      this.background.update(this.context);
+    }
+  }
+  // gameOver(){
+
+  // }
+  checkBorderCollision() {
+    if (
+      this.monkey.y < -80 ||
+      this.monkey.y > 520 ||
+      this.monkey.x < -130 ||
+      this.monkey.x > 935
+    ) {
+      this.monkey.xLeftSpeed = 0;
+      this.monkey.xRightSpeed = 0;
+      this.monkey.gravitySpeed = 0;
+      this.dead();
+    }
+  }
+
+  checkPoint() {
+    this.monkey.y = 200;
+    this.monkey.x = 200;
+    this.fuel.fuelHealth = 300;
+    this.monkey.downThrust = -0.12;
+    this.monkey.xGravity = 0.12;
+    this.monkey.xRightGravity = 0.12;
+  }
+
+  landingCollision(rect1, rect2) {
+    if (
+      rect1.x < rect2.x + rect2.width &&
+      rect1.x + rect1.width > rect2.x &&
+      rect1.y < rect2.y + rect2.height &&
+      rect1.y + rect1.height > rect2.y
+    ) {
+      if (
+        this.monkey.landingClock < 50 &&
+        rect2.x + rect1.width - 28.9 < rect1.x + rect1.width &&
+        rect2.x + rect2.width > rect1.x + rect1.width - 30.24
+      ) {
+        //Stopping the gravity and speed
+        this.monkey.gravitySpeed = 0;
+        this.monkey.gravity = 0;
+        this.monkey.landingClock = 0;
+        this.monkey.xLeftSpeed = 0;
+        this.monkey.xRightSpeed = 0;
+        this.monkey.xGravity = 0;
+        this.monkey.xRightGravity = 0;
+        this.monkey.downThrust = 0;
+        this.monkey.static();
+        this.missionP.update(this.context);
+        if (this.fuel.fuelHealth > 0) {
+          score += 1;
+          this.fuel.decreaseFuel();
         }
-        if(this.state === 2){
-            this.background.update(this.context)
-   
-        }
+      } else {
+        this.dead();
+      }
     }
-    // gameOver(){
-     
-    // }
-    checkBorderCollision(){
-        if(this.monkey.y < -80 || this.monkey.y > 520 || this.monkey.x < -130 || this.monkey.x > 935){
-            this.monkey.xLeftSpeed = 0
-            this.monkey.xRightSpeed = 0
-            this.monkey.gravitySpeed = 0
-            this.dead()
-        }
-    }
+  }
 
-    checkPoint(){
-        this.monkey.y = 200
-        this.monkey.x = 200
-        this.fuel.fuelHealth = 228
-        this.monkey.downThrust = -0.12
-        this.monkey.xGravity = 0.12
-        this.monkey.xRightGravity = 0.12
+  bananaCollision(rect1, rect2) {
+    for (
+      let index = 0;
+      index < this.banana.position[this.level].length;
+      index++
+    ) {
+      // console.log(rect2.position[this.level][index].y)
+      if (
+        rect1.x < rect2.position[this.level][index].x + rect2.width &&
+        rect1.x + rect1.width > rect2.position[this.level][index].x &&
+        rect1.y < rect2.position[this.level][index].y + rect2.height &&
+        rect1.y + rect1.height > rect2.position[this.level][index].y
+      ) {
+        this.banana.position[this.level].splice(index, 1);
+        this.banana.bananaLeftToCollect -= 1;
+        score += 200;
+      }
     }
+  }
 
-    landingCollision(rect1,rect2){
-        if (rect1.x < rect2.x + rect2.width &&
-            rect1.x + rect1.width > rect2.x &&
-            rect1.y < rect2.y + rect2.height &&
-            rect1.y + rect1.height > rect2.y) {
-             if(this.monkey.landingClock < 50 && (rect2.x + rect1.width - 28.9) < (rect1.x + rect1.width) && (rect2.x + rect2.width) > (rect1.x + rect1.width - 30.24)){
-                 //Stopping the gravity and speed 
-                this.monkey.gravitySpeed = 0;
-                this.monkey.gravity = 0;
-                this.monkey.landingClock = 0
-                this.monkey.xLeftSpeed = 0
-                this.monkey.xRightSpeed = 0
-                this.monkey.xGravity = 0
-                this.monkey.xRightGravity = 0
-                this.monkey.downThrust = 0
-                this.monkey.static() 
-                this.missionP.update(this.context)
-                if(this.fuel.fuelHealth >= 0){
-                    score += 1
-                    this.fuel.decreaseFuel()
-                }        
-                }else{
-                 this.dead()
-             }
-         }         
+  dead() {
+    if (this.monkeyLife.monkeyLeft > 0) {
+      this.monkeyLife.monkeyLeft -= 1;
+      this.checkPoint();
+    } else {
+      setTimeout(function () {
+        this.state = 2;
+      }, 100).bind(this); //problem
     }
+  }
 
-    bananaCollision(rect1,rect2){
-        for (let index = 0; index < this.banana.position[this.level].length; index++) {
-            // console.log(rect2.position[this.level][index].y)
-            if (rect1.x < rect2.position[this.level][index].x + rect2.width &&
-                rect1.x + rect1.width > rect2.position[this.level][index].x &&
-                rect1.y < rect2.position[this.level][index].y + rect2.height &&
-                rect1.y + rect1.height > rect2.position[this.level][index].y) {
-                    this.banana.position[this.level].splice(index,1)
-                    this.banana.bananaLeftToCollect -= 1 
-                    score += 200
-                } 
-        }        
-    }
-
-    dead(){
-        if(this.monkeyLife.monkeyLeft > 0){
-            this.monkeyLife.monkeyLeft -= 1
-            this.checkPoint()
-        }else{
-            setTimeout(function(){
-                this.state = 2
-            }, 100).bind(this) //problem 
-        }
-    }
-
-    fuelZero(){
-        // this.
-    }
-
+  fuelZero() {
+    // this.
+  }
 }
 
-let game = new Game(context)
+let game = new Game(context);
 
 // function respawnCondition(){
-    
+
 // }
 
-
-
-
-
-document.addEventListener('keydown',function(e){
-    if(e.key === 'ArrowUp'){
-        game.monkey.up()
-        game.fuel.decreaseFuel()
-    }
-    if(e.key === 'ArrowRight'){
-        game.monkey.right()
-        game.fuel.decreaseFuel()
-
-    }
-    if(e.key === 'ArrowLeft'){
-        game.monkey.left()
-        game.fuel.decreaseFuel()
-    }
-})
-
+document.addEventListener("keydown", function (e) {
+  if (e.key === "ArrowUp") {
+    game.monkey.up();
+    game.fuel.decreaseFuel();
+  }
+  if (e.key === "ArrowRight") {
+    game.monkey.right();
+    game.fuel.decreaseFuel();
+  }
+  if (e.key === "ArrowLeft") {
+    game.monkey.left();
+    game.fuel.decreaseFuel();
+  }
+});
 
 document.onkeyup = function (e) {
-    game.monkey.static()
+  game.monkey.static();
 };
 
-
-
-function gameOn(){
-    game.update()
-    // game.gameOver()
-    requestAnimationFrame(gameOn)
+function gameOn() {
+  game.update();
+  // game.gameOver()
+  requestAnimationFrame(gameOn);
 }
-gameOn()
-
-
+gameOn();
